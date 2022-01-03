5dff865b451a6f88e8852d38e98e8994bc43de70
devjcode <jcodeofficer@gmail.com>
2022-01-03 23:20:14 +0900

    gogo

---
+++ static/js/main_page_function.js
@@ -0,0 +1,18 @@
+function to_main() {
+    location.href = "/"
+}
+
+function to_search() {
+    location.href = '/search'
+}
+
+function to_upload() {
+    location.href = '/write'
+}
+
+function to_favorite() {
+    location.href = "/like"
+}
+
+
+
